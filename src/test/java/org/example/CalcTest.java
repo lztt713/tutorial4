fdec3251554212e94ae56ba7e838479e99aed595
lztt713 <81623555@qq.com>
2025-10-16 18:16:28 +0800

    Add failing subtraction test

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class CalcTest {
   Calc c = new Calc();
-    assertEquals(2, c.subtract(4,2))
+    assertEquals(2, c.subtract(4,2));
     @Test
     void testAddition() {
         assertEquals(4, c.add(2,2));
